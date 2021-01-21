8cf182783c0e0b5c17579a1b63cd997aa1e57136
alhaithamkamal <alhaithamkamalali@gmail.com>
2021-01-22 00:14:54 +0200

    admin views

--- routes/web.php
+++ routes/web.php
@@ -22,5 +22,5 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::get('/admin/dashboard', function() {
-    return view('dashboard');
+    return view('admin.dashboard');
 });
